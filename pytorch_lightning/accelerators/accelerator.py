94ce109afaba6e046b17f99bbf3db6b235496293
SeanNaren <sean.narenthiran@gmail.com>
2020-11-01 11:08:46 +0000

    Swap name to sharded_ddp

--- pytorch_lightning/accelerators/accelerator.py
+++ pytorch_lightning/accelerators/accelerator.py
@@ -242,6 +242,6 @@ class BackendType(Enum):
     # decuple distrib and device
     DDP_CPU = 'ddp_cpu'
     HOROVOD = 'horovod'
-    FAIRSCALE = 'fairscale'
+    DDP_SHARDED = 'ddp_sharded'
     # this is rather device
     TPU = 'tpu'
